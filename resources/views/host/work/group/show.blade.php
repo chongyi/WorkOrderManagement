5de8f931eb992ae256a35b188e523a5990a7223d
chongyi <xpz3847878@163.com>
2015-12-11 23:35:00 +0800

    待调整，准备引入 vue.js

--- resources/views/host/work/group/show.blade.php
+++ resources/views/host/work/group/show.blade.php
@@ -18,18 +18,19 @@
                 <div class="am-panel-bd">
                     <div class="am-btn-toolbar">
                         <div class="am-btn-group am-btn-group-sm">
-                            <a class="am-btn am-btn-success" href="{{ route('host.work.group.work-order.create', [$enableGroup->id]) }}">新建工单</a>
+                            <a class="am-btn am-btn-success"
+                               href="{{ route('host.work.group.work-order.create', [$enableGroup->id]) }}">新建工单</a>
                         </div>
                     </div>
                 </div>
                 <table class="am-table am-table-striped am-table-hover">
                     <thead>
                     <tr>
-                        <td>工单号</td>
-                        <td>工单标题</td>
-                        <td>工单发布人</td>
-                        <td>工单发布日期</td>
-                        <td>操作</td>
+                        <th>工单号</th>
+                        <th>工单标题</th>
+                        <th>工单发布人</th>
+                        <th>工单发布日期</th>
+                        <th>操作</th>
                     </tr>
                     </thead>
                     <tbody id="x-tpl-c-list">
@@ -41,33 +42,9 @@
             </div>
         </div>
     </div>
-    <script type="text/x-mustache-templte" id="x-tpl-list">
-        @{{#list}}
-        <tr>
-            <td>@{{id}}</td>
-            <td>@{{subject}}</td>
-            <td>@{{user}}</td>
-            <td>@{{publish_time}}</td>
-            <td>OPERATE</td>
-        </tr>
-        @{{/list}}
-        @{{^list}}
-        <tr>
-            <td colspan="5">暂无工单记录</td>
-        </tr>
-        @{{/list}}
-    </script>
     <script>
         $(document).ready(function () {
-            $.ajax({
-                url: PAGE_CONFIG.workOrderIndex,
-                type: 'get',
-                dataType: 'json',
-                success: function (response) {
-                    var rendered = Mustache.render($('#x-tpl-list').html(), response.body);
-                    $('#x-tpl-c-list').html(rendered);
-                }
-            });
+            
         });
     </script>
 @stop
