8185ab4885bbf51827ba392531534d4fd9eca224
chongyi <xpz3847878@163.com>
2015-12-14 11:52:12 +0800

    工单变化消息通知

--- resources/views/host/common.blade.php
+++ resources/views/host/common.blade.php
@@ -134,8 +134,7 @@
                         if (headerComponent.$get('message.unread') != response.body) {
                             if (window.Notification){
                                 if(Notification.permission ==='granted'){
-                                    var notification = new Notification('有新的工单动态',{body:"您有新的工单，或者未读消息发生变化，请留意消息变动！"});
-                                    notification.show();
+                                    new Notification('有新的工单动态',{body:"您有新的工单，或者未读消息发生变化，请留意消息变动！"});
                                 }else {
                                     Notification.requestPermission();
                                 }
